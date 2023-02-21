665f59b51793a3cd98dc4eceb6131bdbd98f1ca7
Artyom Yaskuld <98359811+AYaskuld@users.noreply.github.com>
2023-02-21 20:09:03 +0300

    Update daemon_install.sh

--- daemon_install.sh
+++ daemon_install.sh
@@ -6,7 +6,7 @@ sudo cp ./systemd/* /etc/systemd/system/
 sudo touch /home/arplist
 sudo chmod 777 /home/arplist
 sudo chmod 777 ./scrypt.sh
-cp ./scrypt.sh ~/
+sudo cp ./scrypt.sh /root
 
 sudo systemctl enable arpCheck.service
 sudo systemctl enable arpCheck.timer
