9551d7a271ee6319c00c3449e6d7be4250306dc2
Artyom Yaskuld <98359811+AYaskuld@users.noreply.github.com>
2023-03-07 23:51:30 +0300

    Update scrypt.sh

--- scrypt.sh
+++ scrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo echo $(date +'%e.%m.%Y - %H:%M:%S') $(arp -ni eth0|egrep -o "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}|([0-9]{1,3}[\.]){3}[0-9]{1,3}") > /home/arplist
+echo $(date +'%e.%m.%Y - %H:%M:%S') $(arp -ni eth0|egrep -o "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}|([0-9]{1,3}[\.]){3}[0-9]{1,3}") > /home/arplist
